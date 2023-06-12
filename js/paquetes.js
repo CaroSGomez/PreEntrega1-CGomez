81194614b8f9e96cf621d666f40225c90166e8a2
caro.gomez <caroosg26@gmail.com>
2023-06-12 20:28:49 -0300

    Corrijo entrega

--- js/paquetes.js
+++ js/paquetes.js
@@ -1,27 +1,70 @@
-class Paquetes {
-    constructor(destino,dias,salida,precio){
-        this.destino = destino;
-        this.dias = dias;
-        this.salida = salida;
-        this.precio = precio;
-        
-    } 
-    verDetalle(){
-        return `Destino: ${this.destino} <br>
-        Duración: ${this.dias} dias <br>
-        Salidas: ${this.salida} <br>
-        Precio por persona: $${this.precio}`;
-    }
-   
-    
+const ofertas = [
+    {destino:"Salta",
+     duracion:"6 dias",
+     transporte:"Aéreo",
+     hotel:"Amerian Salta",
+     precio:82500,
+     region: 1,
+    },
+    {destino:"San Rafael",
+     duracion:"5 dias",
+     transporte:"Aéreo",
+     hotel:"Terra apart",
+     precio:62500,
+     region: 1,
+
+    },
+    {destino:"Merlo",
+     duracion:"4 dias",
+     transporte:"Bus",
+     hotel:"La estancia",
+     precio:42600,
+     region: 1,
+
+    },
+    {destino:"Bariloche",
+     duracion:"7 dias",
+     transporte:"Aéreo",
+     hotel:"Hotel del lago",
+     precio:92200,
+     region: 1,
+
+    },
+    {destino:"Miami",
+     duracion:"10 dias",
+     transporte:"Aéreo",
+     hotel:"Holiday Inn",
+     precio:782200,
+     region: 2,
+
+    },
+    {destino:"Rio de Janeiro",
+     duracion:"7 dias",
+     transporte:"Aéreo",
+     hotel:"Copacabana Hotel",
+     precio:675200,
+     region: 2,
+    },
+];
+
+let region = prompt("Ingresa 1 si deseas ver ofertas Nacional o 2 para viajar fuera del pais")
+
+while (region >=3){
+    alert("La opción de salida no es válida");
+    region = prompt("Por favor ingresa 1 si deseas ver ofertas Nacional o 2 para viajar fuera del pais")
 }
 
-let sanRafael = new Paquetes("San Rafael",5,"Junio a Julio", 85000)
-let merlo = new Paquetes("Merlo",5,"Junio a Julio", 35000)
-let esteros = new Paquetes("Esteros del Ibera",4,"Mayo a Julio", 45000)
-let ushuaia = new Paquetes("Ushuaia",8,"Julio", 125000)
-let iguazu = new Paquetes("Iguazu",4,"Julio", 75000)
-let salta = new Paquetes("Salta",4,"Agosto a Septiembre", 65000);
+const regionViaje = ofertas.filter((el) => el.region == region)
 
+console.log(regionViaje);
 
-sanRafael.verDetalle();
+const mostrarOpciones= () =>{
+
+    let salida= "";
+        
+    regionViaje.forEach(elemento =>{(salida+=`Destino: ${elemento.destino}.\n Duracion: ${elemento.duracion}. \n Pasaje: ${elemento.transporte}. \n Hotel: ${elemento.hotel}. \n Precio :  $${elemento.precio}. \n \n`)});
+
+    return salida;
+}
+
+alert(`Las ofertas disponibles son: \n\n ${mostrarOpciones()}`);
