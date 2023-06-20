f843893267cf4d650dfa5937beaeb406028828b0
caro.gomez <caroosg26@gmail.com>
2023-06-19 21:43:58 -0300

    Agrego DOM y eventos

--- js/paquetes.js
+++ js/paquetes.js
@@ -1,161 +1,183 @@
 const ofertas = [
-    {id:1,destino:"San Rafael",duracion:"5 dias",transporte:"Aéreo",hotel:"Terra apart",precio:62500,region:1},
-    {id:2,destino:"Merlo",duracion:"4 dias",transporte:"Bus",hotel:"La estancia",precio:42600,region:1},
-    {id:3,destino:"Bariloche",duracion:"7 dias",transporte:"Aéreo",hotel:"Hotel del lago",precio:92200,region:1},
-    {id:4,destino:"Miami",duracion:"6 dias",transporte:"Aéreo", hotel:"Holiday Inn",precio:782200,region:2},
-    {id:5,destino:"Rio de Janeiro",duracion:"7 dias",transporte:"Aéreo",hotel:"Copacabana Hotel",precio:775200,region:2},
-    {id:6,destino:"Cancun",duracion:"8 dias",transporte:"Aéreo",hotel:"Riu Playa del Carmen",precio:1075200,region:2},
+  { id: 1, producto: "vuelo", destino: "Ushuaia", precio: 12500 },
+  { id: 2, producto: "vuelo", destino: "Tucuman", precio: 9000 },
+  { id: 3, producto: "vuelo", destino: "Bariloche", precio: 23000 },
+  { id: 4, producto: "vuelo", destino: "Chapelco", precio: 17000 },
+  { id: 5, producto: "hotel", destino: "Mar del Plata", precio: 6500 },
+  { id: 6, producto: "hotel", destino: "Las grutas", precio: 5000 },
+  { id: 7, producto: "hotel", destino: "Mendoza", precio: 11500 },
+  { id: 8, producto: "paquete", destino: "Sudamerica", precio: 150000 },
+  { id: 9, producto: "crucero", destino: "Caribe", precio: 89000 },
+  { id: 10, producto: "auto", destino: "Miami", precio: 2500 },
 ];
 
-class Usuario{
-    constructor(nombre,apellido,dni,tel,mail,fop){
-        this.nombre=nombre.toUpperCase(),
-        this.apellido=apellido.toUpperCase(),
-        this.dni=dni,
-        this.tel=tel,
-        this.mail=mail,
-        this.fop=fop
-    }
-}
+const paquetes = [
+  {
+    id: 1,
+    imagen:
+      "https://www.argentinaenviaje.com/wp-content/uploads/2020/01/sanrafael-mendoza-med.jpg",
+    destino: "San Rafael",
+    duracion: "5 dias",
+    transporte: "Aéreo",
+    hotel: "Terra apart",
+    precio: 62500,
+    region: 1,
+  },
+  {
+    id: 2,
+    imagen:
+      "https://respiroviajes.com/wp-content/uploads/2020/06/que-hacer-en-Merlo-10.jpg",
+    destino: "Merlo",
+    duracion: "4 dias",
+    transporte: "Bus",
+    hotel: "La estancia",
+    precio: 42600,
+    region: 1,
+  },
+  {
+    id: 3,
+    imagen:
+      "https://media.istockphoto.com/id/820824654/es/foto/bariloche-lago-nahuel-huapi.jpg?s=612x612&w=0&k=20&c=UIe5LkCmk1zAZRFEFnVdAbEncUSPAtpPdRf0KtQHpjo=",
+    destino: "Bariloche",
+    duracion: "7 dias",
+    transporte: "Aéreo",
+    hotel: "Hotel del lago",
+    precio: 92200,
+    region: 1,
+  },
+  {
+    id: 4,
+    imagen:
+      "https://www.barnes-miami.com/wp-content/uploads/2021/03/MG_6_1_Miami.jpg",
+    destino: "Miami",
+    duracion: "6 dias",
+    transporte: "Aéreo",
+    hotel: "Holiday Inn",
+    precio: 782200,
+    region: 2,
+  },
+  {
+    id: 5,
+    imagen:
+      "https://a.cdn-hotels.com/gdcs/production143/d357/42fb6908-dcd5-4edb-9f8c-76208494af80.jpg?impolicy=fcrop&w=800&h=533&q=medium",
+    destino: "Rio de Janeiro",
+    duracion: "7 dias",
+    transporte: "Aéreo",
+    hotel: "Copacabana Hotel",
+    precio: 775200,
+    region: 2,
+  },
+  {
+    id: 6,
+    imagen:
+      "https://www.eleconomista.com.mx/__export/1680217937738/sites/eleconomista/img/2023/03/29/cancun_shutterstock_2114994.jpg_554688468.jpg",
+    destino: "Cancun",
+    duracion: "8 dias",
+    transporte: "Aéreo",
+    hotel: "Riu Playa del Carmen",
+    precio: 1075200,
+    region: 2,
+  },
+];
 
-const usuario1=[];
+const elegidos = [];
 
-function mostrarOpciones() {
+function mostrarPaquetes() {
+  for (const paquete of paquetes) {
+    let div = document.createElement("div");
+    div.className = "col-md-4 text-center";
+    div.innerHTML = `<img src=${paquete.imagen} class="img-fluid">
+          <h3>${paquete.destino}</h3>
+          <p>Paquete de ${paquete.duracion}.<br>
+          Incluye ${paquete.transporte} y alojamiento en ${paquete.hotel}
+        `;
+        const btnInfo = document.createElement("button");
+        btnInfo.classList.add("btn-info");
+        btnInfo.textContent = "Saber costo";
+        btnInfo.onclick = ()=>{
+            mostrarPrecio (paquete.precio);
+        };
 
-    let salida= "";
-        
-    regionViaje.forEach(el =>{(salida+=`Paquete Nro: ${el.id} \n Destino: ${el.destino}.\n Duracion: ${el.duracion}. \n Transporte: ${el.transporte}. \n Hotel: ${el.hotel}. \n Precio por persona: $${el.precio}. \n \n`)});
+    document.getElementById("paquetes").appendChild(div);
+    document.getElementById("paquetes").appendChild(btnInfo);
+    };
+};
 
-    return salida;
-}
+let resultado = document.getElementById("resultado")
 
-function mostrarEleccion(){
-    for (const item of paqueteElegido){
-    alert(`El paquete elegido es \n Paquete Nro: ${item.id} \n Destino: ${item.destino}.\n Duracion: ${item.duracion}. \n Transporte: ${item.transporte}. \n Hotel: ${item.hotel}. \n Precio por persona: $${item.precio}. \n \n`)
-}
-}
+function mostrarPrecio (precio){
+
+    resultado.innerHTML = "";
+    const paqueteElegido = document.createElement("p");
+    paqueteElegido.classList.add("detalle")
+
+    paqueteElegido.textContent = ("El precio por persona en base doble es: $" + precio);
+    paqueteElegido.classList.add("detalle")
+    elegidos.push(paqueteElegido);
+
+    const btnInfoPlus = document.createElement("button");
+    btnInfoPlus.classList.add("btn-info");
+    btnInfoPlus.textContent = "Avanzar con la solicitud";
+    btnInfoPlus.onclick = () => {
+        solicitarDatos();
+    };
+  
+    
+    resultado.appendChild(paqueteElegido);
+    resultado.appendChild(btnInfoPlus);
+    
+};
 
 function solicitarDatos(){
-    let nombre=prompt("Ingresa tu nombre completo");
-    let apellido=prompt("Ingresa tu apellido completo");
-    let dni=prompt("Ingresa tu numero de DNI");
-    let tel=parseInt(prompt("Ingresa tu telefono"));
-    let mail=prompt("Ingresa tu mail");
-    let fop=prompt("Ingresa la opcion de pago deseasa" + "\n" + " 1-Tarjeta de crédito" +"\n"+ "2-Transferencia");
-    if(fop == 1){
-        fop = "Tarjeta de credito"
-    }else if(fop == 2){
-        fop = "Transferencia"
-    };
-
-    usuario1.push(new Usuario(nombre,apellido,dni,tel,mail,fop))
-
+    let datos = document.createElement("div");
+    datos.innerHTML = 
+    `<form class="form" method="GET">
+    <label for="nombre">Nombre</label>
+    <input type="text" id="nombre" />
+    <label for="apellido">Apellido</label>
+    <input type="text" id="apellido" />
+    <label for="mail">Email</label>
+    <input type="email" id="mail" placeholder="carito@gmail.com" />
+    <label for="telefono">Telefono</label>
+    <input type="number" id="telefono" />
+    <label for="text">Nacionalidad</label>
+    <select name="Tipo de usuario">
+      <option value="Cultivador">Residente</option>
+      <option value="Consumidor">Extranjero</option>
+    </select>
+    <label for="texto">Comentarios</label>
+    <textarea
+      name="texto"
+      cols="30"
+      rows="10"
+      placeholder="En qué podemos ayudarte?"
+    ></textarea>
+    <input type="submit" value="Enviar formulario" />
+  </form>
+        `;
+    let nombre= document.getElementById("nombre")
+        
+    resultado.appendChild(datos);
 }
 
-let comprar="";
 
-function mostrarDatos(){
 
-    for (const dato of usuario1){
-        alert(`Verificar datos cargados \n Nombre: ${dato.nombre} \n Apellido: ${dato.apellido}.\n DNI: ${dato.dni}. \n Tel: ${dato.tel}. \n Mail: ${dato.mail}. \n Forma de pago elegida ${dato.fop}. \n \n`)
-    }
-    comprar = prompt("Si los datos estan correctos ingresa 1");
+const guardarLocal = (clave, valor) => {
+  localStorage.setItem(clave, valor);
+};
 
+for (const oferta of ofertas) {
+  guardarLocal(oferta.id, JSON.stringify(oferta));
 }
 
-class Cotizador{
-    constructor(precio,adultos,cuotas){
-      this.precio = precio,
-      this.adultos=adultos,
-      this.cuotas=cuotas
-    }
-  
-    agregarIva(){
-    this.precio = this.precio * 1.21;
-    }
-  
-    mostrarResumen(){
-      document.write("Importe con impuestos por adulto: $" + this.precio + "<br>" + "Cantidad de adultos: "+ this.cuotas + "<br>" + "Cantidad de cuotas elegidas: "+this.cuotas +"<br>")
-    }
-  
-    financiar(){
-      if (this.cuotas == 3){
-        this.cuotas = 1.30
-      } else if(this.cuotas == 6){
-        this.cuotas = 1.36
-      } else if (this.cuotas == 12){
-        this.cuotas = 1.39
-      }
-    }
-  
-    calcularFinal(){
-      let total = (this.precio * this.adultos)*this.cuotas
-      document.write("El importe total del paquete a pagar: $" + total)
-    }
-  
-  }
+guardarLocal("listaProductos", JSON.stringify(ofertas));
 
-let region = prompt("Ingresa 1 si deseas ver ofertas Nacionales o 2 para viajar fuera del pais");
+const almacenados = JSON.parse(localStorage.getItem("listaProductos"));
 
-while ((region >=3) || (region==0) || (region=="")){
-    region = prompt("Por favor ingresa 1 si deseas ver ofertas Nacionales o 2 para viajar fuera del pais")
-}
+console.log(almacenados);
 
-const regionViaje = ofertas.filter((el) => el.region == region)
-
-console.log(regionViaje);
-
-alert(`Las ofertas disponibles son: \n\n ${mostrarOpciones()}`);
-
-let paquete = prompt("Ingresa el nro de paquete que deseas contratar. Marca 0 para volver a ver las opciones disponibles");
-
-while ((paquete == 0)||(paquete=="")){
-    alert(`Las ofertas disponibles son: \n\n ${mostrarOpciones()}`);
-    paquete = prompt("Ingresa el nro de paquete que deseas contratar. Marca 0 para volver a ver las opciones disponibles");
-} 
-
-const paqueteElegido = regionViaje.filter((el)=>el.id == paquete);
-
-mostrarEleccion();
-
-console.log(paqueteElegido)
-
-let precioElegido = paqueteElegido[0].precio
-
-console.log(precioElegido)
-
-let avanzar = prompt("Si deseas avanzar al cotizador de cuotas marca 1. De lo contrario marca 0 para salir")
-let cantidadAdultos;
-let cuotas;
-let venta;
-
-while (avanzar==""){
-    avanzar = prompt("Si deseas avanzar al cotizador de cuotas marca 1. De lo contrario marca 0 para salir")
-}
-if (avanzar == 1){
-    cantidadAdultos = prompt("Ingrese cantidad de adultos");
-    cuotas =prompt("Ingrese la cantidad de cuotas que deseas (3, 6 o 12 cuotas)");
-    while (cuotas == ""){
-      alert("La opción de salida no es válida");
-      cuotas =prompt("Ingrese la cantidad de cuotas elegidas(3, 6 o 12 cuotas)")
-  }
-    cuotas = parseInt(cuotas)
-    const calculo1 = new Cotizador(precioElegido,cantidadAdultos,cuotas)
-
-    calculo1.agregarIva();
-    calculo1.mostrarResumen();
-    calculo1.financiar();
-    calculo1.calcularFinal();
-
-    alert("Gracias por usar nuestro cotizador")
-    
-} else if(avanzar == 0){
-     venta = prompt("Tal vez alguno de nuestros agentes de viajes pueda ayudarte! Si deseas que te comuniquen presiona 1 y completa los datos. De lo contrario presiona cualquier tecla para salir");
-     if(venta == 1){
-        solicitarDatos();
-        mostrarDatos();
-        alert(`En el trancurso del dia de hoy un agente de ventas te contactará para concretar tu viaje`)
-     } 
-}
+document.addEventListener("DOMContentLoaded", () => {
+  mostrarPaquetes();
+});
 
 
